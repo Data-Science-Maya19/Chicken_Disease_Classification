275fc44a41630682024df9daf27975a1d6f6c16a
Data-Science-Maya19 <mayura19.zadane@gmail.com>
2026-01-25 22:18:47 -0500

    Base Model Created VGG16

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from CNN_Classifier import logger
 from CNN_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from CNN_Classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f"----- Stage {STAGE_NAME} started ------")
     obj = DataIngestionTrainingPipeline()
     obj.main()
-    logger.info(f"----- Stage {STAGE_NAME} completed ------\n\nx")
+    logger.info(f"----- Stage {STAGE_NAME} completed ------\n\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare Base Model"
+try:
+    logger.info(f"----- Stage {STAGE_NAME} started ------")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f"----- Stage {STAGE_NAME} completed ------\n\n")
 except Exception as e:
     logger.exception(e)
     raise e
