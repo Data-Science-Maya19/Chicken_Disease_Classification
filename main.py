ecda173aa1f8355cc7f40e056ef3543b531708e2
Data-Science-Maya19 <mayura19.zadane@gmail.com>
2026-03-12 17:18:14 -0400

    dvc added

--- main.py
+++ main.py
@@ -2,35 +2,55 @@ from CNN_Classifier import logger
 from CNN_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from CNN_Classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from CNN_Classifier.pipeline.stage_03_training import ModelTrainingPipeline
+from CNN_Classifier.pipeline.stage_04_evaluation import EvaluationPipeline
 
-STAGE_NAME = "Data Ingestion Stage"
+
+STAGE_NAME = "Data Ingestion stage"
 try:
-    logger.info(f"----- Stage {STAGE_NAME} started ------")
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
-    logger.info(f"----- Stage {STAGE_NAME} completed ------\n\n")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-    logger.exception(e)
-    raise e
+        logger.exception(e)
+        raise e
 
 
-STAGE_NAME = "Prepare Base Model"
-try:
-    logger.info(f"----- Stage {STAGE_NAME} started ------")
-    obj = PrepareBaseModelTrainingPipeline()
-    obj.main()
-    logger.info(f"----- Stage {STAGE_NAME} completed ------\n\n")
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-    logger.exception(e)
-    raise e
+        logger.exception(e)
+        raise e
+
 
 
 STAGE_NAME = "Training"
-try:
-    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
-    obj = ModelTrainingPipeline()
-    obj.main()
-    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-    logger.exception(e)
-    raise e
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/CNN_Classifier/entity/config_entity.py
+++ src/CNN_Classifier/entity/config_entity.py
@@ -38,3 +38,12 @@ class TrainingConfig:
     params_in_augumentation: bool
     params_image_size: list
     params_learning_rate: float
+
+
+@dataclass(frozen=True)
+class EvaluationConfig:
+    path_of_model: Path
+    training_data: Path
+    all_params: dict
+    params_image_size: list
+    params_batch_size: int
